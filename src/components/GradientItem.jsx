a31cb2f4d904719183f0effef32b25c5366aea5f
Alina Dereviankina <fion4egg@gmail.com>
2020-09-01 23:10:34 +0800

    prettier

--- src/components/GradientItem.jsx
+++ src/components/GradientItem.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 
-const GradientItem = props => {
+const GradientItem = (props) => {
+  const { inputValueSecond, inputValueFirst, isSubmitted } = props;
 
-    const { inputValueSecond, inputValueFirst, isSubmitted } = props;
+  const itemStyle = {
+    width: "300px",
+    height: "100px",
+    margin: "30px auto",
+    backgroundImage: `linear-gradient(${props.inputValueFirst}, ${props.inputValueSecond})`,
+  };
 
-    const itemStyle = {
-        width: "300px",
-        height: "100px",
-        margin: "30px auto",
-        backgroundImage: `linear-gradient(${props.inputValueFirst}, ${props.inputValueSecond})`
-    };
-
-    console.log('props ', props, itemStyle);
-
-    return (
-        <div style={itemStyle}>
-            <p>lolo</p>
-            <button>remove</button>
-        </div>
-    )};
+  return (
+    <div style={itemStyle}>
+      <button>remove</button>
+    </div>
+  );
+};
 
 export default GradientItem;
