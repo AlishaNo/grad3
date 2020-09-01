ab172cd2b6a9dcfafb3a32dea8fae042968af486
Alina Dereviankina <fion4egg@gmail.com>
2020-09-01 19:06:23 +0800

    Updates

---
+++ precache-manifest.a0201ef00a3e8abc03bdbfc52d931cfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a69ed8a18f8d9f5155081336e5f23dd",
+    "url": "/grad3/index.html"
+  },
+  {
+    "revision": "6a57af965b9feb9f5f60",
+    "url": "/grad3/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "44434bdf36b0c9d6dd11",
+    "url": "/grad3/static/js/2.46cd5b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grad3/static/js/2.46cd5b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a57af965b9feb9f5f60",
+    "url": "/grad3/static/js/main.907440a5.chunk.js"
+  },
+  {
+    "revision": "974ec92033c812c12889",
+    "url": "/grad3/static/js/runtime-main.2d896fbe.js"
+  }
+]);
