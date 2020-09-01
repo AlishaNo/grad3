fca24d8c0fe32b4ae042db957b539127c7132175
Alina Dereviankina <fion4egg@gmail.com>
2020-09-02 02:54:16 +0800

    fix review notes

--- src/components/GradientItem.jsx
+++ src/components/GradientItem.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 const GradientItem = (props) => {
-  const { inputValueSecond, inputValueFirst, isSubmitted } = props;
-
+  const { colorSet } = props;
+  
   const itemStyle = {
     width: "300px",
     height: "100px",
     margin: "30px auto",
-    backgroundImage: `linear-gradient(${props.inputValueFirst}, ${props.inputValueSecond})`,
+    backgroundImage: `linear-gradient(${colorSet.primary}, ${colorSet.secondary})`,
   };
-
+  
   return (
-    <div style={itemStyle}>
-      <button>remove</button>
-    </div>
+      <div style={itemStyle}>
+        <button>remove</button>
+      </div>
   );
 };
 
